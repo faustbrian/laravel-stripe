a97c27d44485969dd27d10b35e5eb0aa068d1dc4
Brian Faust <hello@brianfaust.de>
2016-12-03 06:32:42 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/StripeFactory.php
+++ src/StripeFactory.php
@@ -45,7 +45,7 @@ class StripeFactory
         $keys = ['key'];
 
         foreach ($keys as $key) {
-            if (! array_key_exists($key, $config)) {
+            if (!array_key_exists($key, $config)) {
                 throw new InvalidArgumentException("Missing configuration key [$key].");
             }
         }
